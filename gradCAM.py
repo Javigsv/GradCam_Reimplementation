7b7b0734e59ece346b36577367912589f3b5e0ba
Javier García San Vicente <javigarciasv@gmail.com>
2020-10-13 15:57:24 +0200

    Update gradCAM.py

--- gradCAM.py
+++ gradCAM.py
@@ -26,12 +26,14 @@ class GradCAM:
                 c = np.argmax(predictions[0])
             c = int(c)
             score = predictions[:,c]
-                
+        
+        top_c = np.flip(np.array(predictions).argsort()[0][-10:])
+
         grads = t.gradient(score, featureMaps)
         alpha_c_k = tf.reduce_mean(grads, axis=(1,2))
         locMap = np.maximum(tf.reduce_sum(tf.multiply(alpha_c_k, featureMaps), axis=3).numpy()[0], 0)
         # 14 x 14 Map
-        return locMap, c
+        return locMap, c, top_c
 
     def getHeatmap(self, locMap, image):
         locMapResized = cv2.resize(locMap, (224, 224))
@@ -63,41 +65,74 @@ def parseArgs():
 
     return parser.parse_args()
 
-def main():
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-    args = parseArgs()
+def mainMultipleImages(args):
 
     model = VGG16(weights='imagenet')
 
     gradCAM = GradCAM(model, args.layer)
 
     labelsMap = pickle.load(open('labelsMap.p', 'rb'))
-    classMap = pickle.load(open('classMap.p', 'rb'))
     resultsFile = open('./classRelation.txt', 'w')
 
-    if not args.imagePath == 'None':
-        path = args.imagePath
-        image = preProcessImage(path)
-        c = classMap.get(args.imageClass, args.imageClass)
-        locMap, c = gradCAM.getLocalizationMap(image, c)
-        heatMap = gradCAM.getHeatmap(locMap, image)
-        name = os.path.split(path)[-1]
-        plt.imsave(args.resultsPath + args.layer + '_' + str(c) + '_' + name, np.uint8(heatMap))
-
-    else:
-
-        for root, dirs, files in os.walk(args.dataPath):
+    for root, dirs, files in os.walk(args.dataPath):
             for name in files:
                 path = os.path.join(root, name)
                 image = preProcessImage(path)
-                locMap, c = gradCAM.getLocalizationMap(image)
+                locMap, c, _ = gradCAM.getLocalizationMap(image)
                 heatMap = gradCAM.getHeatmap(locMap, image)
                 
                 resultsFile.write(path + ' ---> ' + labelsMap[c] + '\n')
                 plt.imsave(args.resultsPath + args.layer + '_' + name, np.uint8(heatMap))
 
     resultsFile.close()
+
+def mainSimpleImage(args):
+
+    model = VGG16(weights='imagenet')
+    
+    gradCAM = GradCAM(model, args.layer)
+
+    classMap = pickle.load(open('classMap.p', 'rb'))
+
+    path = args.imagePath
+    image = preProcessImage(path)
+    c = classMap.get(args.imageClass, args.imageClass)
+    locMap, c, top_c = gradCAM.getLocalizationMap(image, c)
+    heatMap = gradCAM.getHeatmap(locMap, image)
+    name = os.path.split(path)[-1]
+    plt.imsave(args.resultsPath + args.layer + '_' + str(c) + '_' + name, np.uint8(heatMap))
+
+    labelsMap = pickle.load(open('labelsMap.p', 'rb'))
+    if not True:
+        for i,t in enumerate(top_c):
+            print(str(i) + '.    ' + str(t) + ' ---> ' + labelsMap[t] + '\n')
+
+
+def mainMultipleLayers(args):
+
+    layers = ['block1_conv1', 'block1_conv2','block1_pool','block2_conv1','block2_conv2','block2_pool','block3_conv1','block3_conv2','block3_conv3','block3_pool','block4_conv1','block4_conv2','block4_conv3','block4_pool','block5_conv1','block5_conv2','block5_conv3','block5_pool']
+    
+    for layer in layers:
+        print('Computing map for layer', layer,'...')
+        args.layer = layer
+        mainSimpleImage(args)
+
+def main():
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    args = parseArgs()
+
+    if not args.imagePath == 'None':
+        if args.layer == 'list':
+            mainMultipleLayers(args)
+        else:
+            mainSimpleImage(args)
+    else:
+        mainMultipleImages(args)
+
+
+        
 
 if __name__ == "__main__":
     main()
