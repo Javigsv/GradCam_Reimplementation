0c0725fa5c8dd1b5e7cb1830f28a25a353e5f9df
FlaviaGarcia <flavi13a@gmail.com>
2020-10-16 17:14:43 +0200

    code refactorization

---
+++ GradCAMPlusPlus.py
@@ -0,0 +1,29 @@
+from GradCAM import GradCAM
+from tensorflow.keras.models import Model
+import tensorflow as tf 
+import numpy as np 
+
+
+class GradCAMPlusPlus(GradCAM):
+    def __init__(self, model, layer):
+        super().__init__(model, layer)
+
+    def getLocalizationMap(self, image, c = 'None'):
+        gradModel = Model(inputs = self.model.inputs, outputs = [self.model.get_layer(self.layer).output, self.model.output])
+        with tf.GradientTape() as t:
+            (featureMaps, predictions) = gradModel(image)
+            if c == None:
+                c = np.argmax(predictions[0])
+            c = int(c)
+            score = predictions[:,c]
+        grads = t.gradient(score, featureMaps)
+        first_y = np.exp(score)*grads
+        second_y = np.exp(score)*grads*grads
+        third_y = np.exp(score)*grads*grads*grads
+        alpha_c_k_i_j_dem = second_y*2 + tf.reduce_sum(featureMaps)*third_y
+        alpha_denom = np.where(alpha_c_k_i_j_dem != 0.0, alpha_c_k_i_j_dem, np.ones(alpha_c_k_i_j_dem.shape))
+        alpha_c_k_i_j = second_y / alpha_denom
+        w_c_k = tf.reduce_sum(alpha_c_k_i_j*np.maximum(0,grads.numpy()[0]), axis=(1,2))
+        locMap = np.maximum(tf.reduce_sum(tf.multiply(w_c_k, featureMaps), axis=3).numpy()[0], 0)
+        # 14 x 14 Map
+        return locMap, c
--- imageNet_experiments.py
+++ imageNet_experiments.py
@@ -7,91 +7,12 @@ import pickle
 import tensorflow as tf
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 from tensorflow.keras.applications.resnet import ResNet50, preprocess_input, decode_predictions
-from tensorflow.keras.models import Model
+
 import tensorflow.keras.preprocessing.image as I
 from time import gmtime, asctime
 
-class GradCAM:
-    def __init__(self, model, layer):
-        self.model = model
-        self.layer = layer
-
-    def getLocalizationMap(self, image, c = 'None'):
-        gradModel = Model(inputs = self.model.inputs, outputs = [self.model.get_layer(self.layer).output, self.model.output])
-        with tf.GradientTape() as t:
-            (featureMaps, predictions) = gradModel(image)
-            if c == None:
-                c = np.argmax(predictions[0])
-            c = int(c)
-            score = predictions[:,c]
-        
-        top_c = np.flip(np.array(predictions).argsort()[0][-10:])
-
-        grads = t.gradient(score, featureMaps)
-        alpha_c_k = tf.reduce_mean(grads, axis=(1,2))
-        locMap = np.maximum(tf.reduce_sum(tf.multiply(alpha_c_k, featureMaps), axis=3).numpy()[0], 0)
-        # 14 x 14 Map
-        return locMap, c, top_c
-
-    def getHeatmap(self, locMap, image):
-        locMapResized = cv2.resize(locMap, (224, 224))
-        heatmap = locMapResized / np.max(locMapResized)
-        colorMap = cv2.applyColorMap(np.uint8(heatmap*255), cv2.COLORMAP_JET)
-        cm_rgb = cv2.cvtColor(colorMap, cv2.COLOR_BGR2RGB)
-        image = image[0, :]
-        image -= np.min(image)
-        image = np.minimum(image, 255)
-        overLayed = cm_rgb + image
-        overLayed = 255 * overLayed / np.max(overLayed)
-        return overLayed
-    
-    def evaluate(self, locMap, imagePath, c):
-        image = I.load_img(imagePath,target_size=(224,224))
-        image = I.img_to_array(image)
-        locMapResized = cv2.resize(locMap, (224, 224))
-        heatmap = locMapResized / np.max(locMapResized)
-        _, thresh_img = cv2.threshold(np.uint8(heatmap*255), 30, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        black_img = np.zeros(image.shape).astype(image.dtype)
-        color = [1, 1, 1]
-        cv2.fillPoly(black_img, contours, color)
-        segmented_img = image*black_img
-        segmented_img = np.reshape(segmented_img,(1, 224,224,3))
-        segmented_img = preprocess_input(segmented_img)
-        original_img = np.reshape(image,(1, 224,224,3))
-        original_img = preprocess_input(original_img)
-        Y = self.model(original_img)[0,c].numpy()
-        O = self.model(segmented_img)[0,c].numpy()
-        drop = np.maximum(0,Y-O)/Y
-        inc = 0
-        if Y < O:
-            inc = 1
-        return drop, inc
-
-
-class GradCAMPlusPlus(GradCAM):
-    def __init__(self, model, layer):
-        super().__init__(model, layer)
-
-    def getLocalizationMap(self, image, c = 'None'):
-        gradModel = Model(inputs = self.model.inputs, outputs = [self.model.get_layer(self.layer).output, self.model.output])
-        with tf.GradientTape() as t:
-            (featureMaps, predictions) = gradModel(image)
-            if c == None:
-                c = np.argmax(predictions[0])
-            c = int(c)
-            score = predictions[:,c]
-        grads = t.gradient(score, featureMaps)
-        first_y = np.exp(score)*grads
-        second_y = np.exp(score)*grads*grads
-        third_y = np.exp(score)*grads*grads*grads
-        alpha_c_k_i_j_dem = second_y*2 + tf.reduce_sum(featureMaps)*third_y
-        alpha_denom = np.where(alpha_c_k_i_j_dem != 0.0, alpha_c_k_i_j_dem, np.ones(alpha_c_k_i_j_dem.shape))
-        alpha_c_k_i_j = second_y / alpha_denom
-        w_c_k = tf.reduce_sum(alpha_c_k_i_j*np.maximum(0,grads.numpy()[0]), axis=(1,2))
-        locMap = np.maximum(tf.reduce_sum(tf.multiply(w_c_k, featureMaps), axis=3).numpy()[0], 0)
-        # 14 x 14 Map
-        return locMap, c
+from GradCAM import GradCAM
+from GradCAMPlusPlus import GradCAMPlusPlus
 
 def preProcessImage(imagePath):
     image = I.load_img(imagePath, target_size=(224,224))
@@ -99,6 +20,7 @@ def preProcessImage(imagePath):
     image = np.reshape(image,(1, 224,224,3))
     image = preprocess_input(image)
     return image
+
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='GradCAM')
